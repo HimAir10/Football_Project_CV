033c9b48feb37e9f20becccc12f183523ee5adff
Himanshu Jha <himanshujha@Himanshus-MacBook-Air-2.local>
2025-07-11 00:56:18 +0530

    Initial commit

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import sys
 from ultralytics import YOLO
 from sklearn.cluster import KMeans
 
+
 def get_grass_color(img):
     """
     Finds the color of the grass in the background of the image
@@ -31,149 +32,158 @@ def get_grass_color(img):
     grass_color = cv2.mean(img, mask=mask)
     return grass_color[:3]
 
+
 def get_players_boxes(result):
-  """
-  Finds the images of the players in the frame and their bounding boxes.
+    """
+    Finds the images of the players in the frame and their bounding boxes.
 
-  Args:
-      result: ultralytics.engine.results.Results object that contains all the
-      result of running the object detection algroithm on the frame
+    Args:
+        result: ultralytics.engine.results.Results object that contains all the
+        result of running the object detection algroithm on the frame
 
-  Returns:
-      players_imgs
-          List of np.array objects that contain the BGR values of the cropped
-          parts of the image that contains players.
-      players_boxes
-          List of ultralytics.engine.results.Boxes objects that contain various
-          information about the bounding boxes of the players found in the image.
-  """
-  players_imgs = []
-  players_boxes = []
-  for box in result.boxes:
-    label = int(box.cls.numpy()[0])
-    if label == 0:
-      x1, y1, x2, y2 = map(int, box.xyxy[0].numpy())
-      player_img = result.orig_img[y1: y2, x1: x2]
-      players_imgs.append(player_img)
-      players_boxes.append(box)
-  return players_imgs, players_boxes
+    Returns:
+        players_imgs
+            List of np.array objects that contain the BGR values of the cropped
+            parts of the image that contains players.
+        players_boxes
+            List of ultralytics.engine.results.Boxes objects that contain various
+            information about the bounding boxes of the players found in the image.
+    """
+    players_imgs = []
+    players_boxes = []
+    for box in result.boxes:
+        label = int(box.cls.cpu().numpy()[0])
+
+        if label == 0:
+            x1, y1, x2, y2 = map(int, box.xyxy[0].cpu().numpy())
+            player_img = result.orig_img[y1: y2, x1: x2]
+            players_imgs.append(player_img)
+            players_boxes.append(box)
+    return players_imgs, players_boxes
+
 
 def get_kits_colors(players, grass_hsv=None, frame=None):
-  """
-  Finds the kit colors of all the players in the current frame
+    """
+    Finds the kit colors of all the players in the current frame
 
-  Args:
-      players: List of np.array objects that contain the BGR values of the image
-      portions that contain players.
-      grass_hsv: tuple that contain the HSV color value of the grass color of
-      the image background.
+    Args:
+        players: List of np.array objects that contain the BGR values of the image
+        portions that contain players.
+        grass_hsv: tuple that contain the HSV color value of the grass color of
+        the image background.
 
-  Returns:
-      kits_colors
-          List of np arrays that contain the BGR values of the kits color of all
-          the players in the current frame
-  """
-  kits_colors = []
-  if grass_hsv is None:
-	  grass_color = get_grass_color(frame)
-	  grass_hsv = cv2.cvtColor(np.uint8([[list(grass_color)]]), cv2.COLOR_BGR2HSV)
+    Returns:
+        kits_colors
+            List of np arrays that contain the BGR values of the kits color of all
+            the players in the current frame
+    """
+    kits_colors = []
+    if grass_hsv is None:
+        grass_color = get_grass_color(frame)
+        grass_hsv = cv2.cvtColor(np.uint8([[list(grass_color)]]), cv2.COLOR_BGR2HSV)
 
-  for player_img in players:
-      # Convert image to HSV color space
-      hsv = cv2.cvtColor(player_img, cv2.COLOR_BGR2HSV)
+    for player_img in players:
+        # Convert image to HSV color space
+        hsv = cv2.cvtColor(player_img, cv2.COLOR_BGR2HSV)
 
-      # Define range of green color in HSV
-      lower_green = np.array([grass_hsv[0, 0, 0] - 10, 40, 40])
-      upper_green = np.array([grass_hsv[0, 0, 0] + 10, 255, 255])
+        # Define range of green color in HSV
+        lower_green = np.array([grass_hsv[0, 0, 0] - 10, 40, 40])
+        upper_green = np.array([grass_hsv[0, 0, 0] + 10, 255, 255])
 
-      # Threshold the HSV image to get only green colors
-      mask = cv2.inRange(hsv, lower_green, upper_green)
+        # Threshold the HSV image to get only green colors
+        mask = cv2.inRange(hsv, lower_green, upper_green)
 
-      # Bitwise-AND mask and original image
-      mask = cv2.bitwise_not(mask)
-      upper_mask = np.zeros(player_img.shape[:2], np.uint8)
-      upper_mask[0:player_img.shape[0]//2, 0:player_img.shape[1]] = 255
-      mask = cv2.bitwise_and(mask, upper_mask)
+        # Bitwise-AND mask and original image
+        mask = cv2.bitwise_not(mask)
+        upper_mask = np.zeros(player_img.shape[:2], np.uint8)
+        upper_mask[0:player_img.shape[0] // 2, 0:player_img.shape[1]] = 255
+        mask = cv2.bitwise_and(mask, upper_mask)
 
-      kit_color = np.array(cv2.mean(player_img, mask=mask)[:3])
+        kit_color = np.array(cv2.mean(player_img, mask=mask)[:3])
 
-      kits_colors.append(kit_color)
-  return kits_colors
+        kits_colors.append(kit_color)
+    return kits_colors
+
 
 def get_kits_classifier(kits_colors):
-  """
-  Creates a K-Means classifier that can classify the kits accroding to their BGR
-  values into 2 different clusters each of them represents one of the teams
+    """
+    Creates a K-Means classifier that can classify the kits accroding to their BGR
+    values into 2 different clusters each of them represents one of the teams
 
-  Args:
-      kits_colors: List of np.array objects that contain the BGR values of
-      the colors of the kits of the players found in the current frame.
+    Args:
+        kits_colors: List of np.array objects that contain the BGR values of
+        the colors of the kits of the players found in the current frame.
 
-  Returns:
-      kits_kmeans
-          sklearn.cluster.KMeans object that can classify the players kits into
-          2 teams according to their color..
-  """
-  kits_kmeans = KMeans(n_clusters=2)
-  kits_kmeans.fit(kits_colors);
-  return kits_kmeans
+    Returns:
+        kits_kmeans
+            sklearn.cluster.KMeans object that can classify the players kits into
+            2 teams according to their color..
+    """
+    if not kits_colors or len(kits_colors) < 2:
+        raise ValueError(f"Insufficient kit colors for clustering. Received: {kits_colors}")
+    kits_kmeans = KMeans(n_clusters=2)
+    kits_kmeans.fit(kits_colors);
+    return kits_kmeans
+
 
 def classify_kits(kits_classifer, kits_colors):
-  """
-  Classifies the player into one of the two teams according to the player's kit
-  color
+    """
+    Classifies the player into one of the two teams according to the player's kit
+    color
 
-  Args:
-      kits_classifer: sklearn.cluster.KMeans object that can classify the
-      players kits into 2 teams according to their color.
-      kits_colors: List of np.array objects that contain the BGR values of
-      the colors of the kits of the players found in the current frame.
+    Args:
+        kits_classifer: sklearn.cluster.KMeans object that can classify the
+        players kits into 2 teams according to their color.
+        kits_colors: List of np.array objects that contain the BGR values of
+        the colors of the kits of the players found in the current frame.
 
-  Returns:
-      team
-          np.array object containing a single integer that carries the player's
-          team number (0 or 1)
-  """
-  team = kits_classifer.predict(kits_colors)
-  return team
+    Returns:
+        team
+            np.array object containing a single integer that carries the player's
+            team number (0 or 1)
+    """
+    team = kits_classifer.predict(kits_colors)
+    return team
+
 
 def get_left_team_label(players_boxes, kits_colors, kits_clf):
-  """
-  Finds the label of the team that is on the left of the screen
+    """
+    Finds the label of the team that is on the left of the screen
 
-  Args:
-      players_boxes: List of ultralytics.engine.results.Boxes objects that
-      contain various information about the bounding boxes of the players found
-      in the image.
-      kits_colors: List of np.array objects that contain the BGR values of
-      the colors of the kits of the players found in the current frame.
-      kits_clf: sklearn.cluster.KMeans object that can classify the players kits
-      into 2 teams according to their color.
-  Returns:
-      left_team_label
-          Int that holds the number of the team that's on the left of the image
-          either (0 or 1)
-  """
-  left_team_label = 0
-  team_0 = []
-  team_1 = []
+    Args:
+        players_boxes: List of ultralytics.engine.results.Boxes objects that
+        contain various information about the bounding boxes of the players found
+        in the image.
+        kits_colors: List of np.array objects that contain the BGR values of
+        the colors of the kits of the players found in the current frame.
+        kits_clf: sklearn.cluster.KMeans object that can classify the players kits
+        into 2 teams according to their color.
+    Returns:
+        left_team_label
+            Int that holds the number of the team that's on the left of the image
+            either (0 or 1)
+    """
+    left_team_label = 0
+    team_0 = []
+    team_1 = []
 
-  for i in range(len(players_boxes)):
-    x1, y1, x2, y2 = map(int, players_boxes[i].xyxy[0].numpy())
+    for i in range(len(players_boxes)):
+        x1, y1, x2, y2 = map(int, players_boxes[i].xyxy[0].numpy())
 
-    team = classify_kits(kits_clf, [kits_colors[i]]).item()
-    if team==0:
-      team_0.append(np.array([x1]))
-    else:
-      team_1.append(np.array([x1]))
+        team = classify_kits(kits_clf, [kits_colors[i]]).item()
+        if team == 0:
+            team_0.append(np.array([x1]))
+        else:
+            team_1.append(np.array([x1]))
 
-  team_0 = np.array(team_0)
-  team_1 = np.array(team_1)
+    team_0 = np.array(team_0)
+    team_1 = np.array(team_1)
 
-  if np.average(team_0) - np.average(team_1) > 0:
-    left_team_label = 1
+    if np.average(team_0) - np.average(team_1) > 0:
+        left_team_label = 1
 
-  return left_team_label
+    return left_team_label
+
 
 def annotate_video(video_path, model):
     """
@@ -192,10 +202,13 @@ def annotate_video(video_path, model):
 
     video_name = video_path.split('/')[-1]
     fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-    output_video = cv2.VideoWriter('./output/'+video_name.split('.')[0] + "_out.mp4",
-                                   fourcc,
-                                   30.0,
-                                   (width, height))
+    import os
+
+    output_dir = './output'
+    os.makedirs(output_dir, exist_ok=True)
+
+    output_path = os.path.join(output_dir, video_name.split('.')[0] + "_out.mp4")
+    output_video = cv2.VideoWriter(output_path, fourcc, 30.0, (width, height))
 
     kits_clf = None
     left_team_label = 0
@@ -208,7 +221,7 @@ def annotate_video(video_path, model):
         current_frame_idx = cap.get(cv2.CAP_PROP_POS_FRAMES)
         if success:
 
-            # Run YOLOv8 inference on the frame
+            # Run YOLOv11 inference on the frame
             annotated_frame = cv2.resize(frame, (width, height))
             result = model(annotated_frame, conf=0.5, verbose=False)[0]
 
@@ -218,38 +231,74 @@ def annotate_video(video_path, model):
 
             # Run on the first frame only
             if current_frame_idx == 1:
-                kits_clf = get_kits_classifier(kits_colors)
-                left_team_label = get_left_team_label(players_boxes, kits_colors, kits_clf)
-                grass_color = get_grass_color(result.orig_img)
-                grass_hsv = cv2.cvtColor(np.uint8([[list(grass_color)]]), cv2.COLOR_BGR2HSV)
-
-            for box in result.boxes:
-                label = int(box.cls.numpy()[0])
-                x1, y1, x2, y2 = map(int, box.xyxy[0].numpy())
-
-                # If the box contains a player, find to which team he belongs
-                if label == 0:
-                    kit_color = get_kits_colors([result.orig_img[y1: y2, x1: x2]], grass_hsv)
-                    team = classify_kits(kits_clf, kit_color)
-                    if team == left_team_label:
-                        label = 0
-                    else:
-                        label = 1
-
-                # If the box contains a Goalkeeper, find to which team he belongs
-                elif label == 1:
-                    if x1 < 0.5 * width:
-                        label = 2
-                    else:
-                        gk_label = 3
-
-                # Increase the label by 2 because of the two add labels "Player-L", "GK-L"
+                if len(players_imgs) >= 2 and len(kits_colors) >= 2:
+                    kits_clf = get_kits_classifier(kits_colors)
+                    left_team_label = get_left_team_label(players_boxes, kits_colors, kits_clf)
+                    grass_color = get_grass_color(result.orig_img)
+                    grass_hsv = cv2.cvtColor(np.uint8([[list(grass_color)]]), cv2.COLOR_BGR2HSV)
                 else:
-                    label = label + 2
+                    print(f"Frame {current_frame_idx}: Not enough players or kit colors to build classifier.")
+                    # Skip processing boxes if classifier isn't ready
+                    output_video.write(annotated_frame)
+                    continue
 
-                cv2.rectangle(annotated_frame, (x1, y1), (x2, y2), box_colors[str(label)], 2)
-                cv2.putText(annotated_frame, labels[label], (x1 - 30, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9,
-                            box_colors[str(label)], 2)
+            # Only process boxes if classifier is ready
+            if kits_clf is not None:
+                for box in result.boxes:
+                    label = int(box.cls.cpu().numpy()[0])
+
+                    x1, y1, x2, y2 = map(int, box.xyxy[0].cpu().numpy())
+
+                    # If the box contains a player, find to which team he belongs
+                    if label == 0:
+                        kit_color = get_kits_colors([result.orig_img[y1: y2, x1: x2]], grass_hsv)
+                        team = classify_kits(kits_clf, kit_color)
+                        if team == left_team_label:
+                            label = 0
+                        else:
+                            label = 1
+
+
+                    elif label == 1:
+                        if x1 < 0.5 * width:
+                            label = 2
+                        else:
+                            label = 3
+
+
+                    else:
+                        label = label + 2
+
+                    # Safety check to prevent index out of range
+                    if label < len(labels):
+                        cv2.rectangle(annotated_frame, (x1, y1), (x2, y2), box_colors[str(label)], 2)
+                        cv2.putText(annotated_frame, labels[label], (x1 - 30, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9,
+                                    box_colors[str(label)], 2)
+                    else:
+                        print(f"⚠️ Label {label} is out of range. Max available: {len(labels) - 1}")
+            else:
+                # If classifier not ready, just draw basic boxes without team classification
+                for box in result.boxes:
+                    label = int(box.cls.cpu().numpy()[0])
+                    x1, y1, x2, y2 = map(int, box.xyxy[0].cpu().numpy())
+
+                    # Use original labels for non-player/GK objects, or generic labels for players
+                    if label == 0:
+                        display_label = "Player"
+                        color = (255, 255, 255)
+                    elif label == 1:
+                        display_label = "GK"
+                        color = (128, 128, 128)
+                    elif label < len(labels) - 2:
+                        display_label = labels[label + 2]
+                        color = box_colors.get(str(label + 2), (255, 255, 255))
+                    else:
+                        display_label = f"Class_{label}"
+                        color = (255, 255, 255)
+
+                    cv2.rectangle(annotated_frame, (x1, y1), (x2, y2), color, 2)
+                    cv2.putText(annotated_frame, display_label, (x1 - 30, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9,
+                                color, 2)
 
             # Write the annotated frame
             output_video.write(annotated_frame)
@@ -258,13 +307,24 @@ def annotate_video(video_path, model):
             # Break the loop if the end of the video is reached
             break
 
-    cv2.destroyAllWindows()
     output_video.release()
     cap.release()
 
+
 if __name__ == "__main__":
 
-    labels = ["Player-L", "Player-R", "GK-L", "GK-R", "Ball", "Main Ref", "Side Ref", "Staff"]
+    labels = [
+        "Player-L",  # 0 - Left team player
+        "Player-R",  # 1 - Right team player
+        "GK-L",  # 2 - Left team goalkeeper
+        "GK-R",  # 3 - Right team goalkeeper
+        "Ball",  # 4 - Ball (original label 2 + 2)
+        "Main Ref",  # 5 - Main Referee (original label 3 + 2)
+        "Side Ref",  # 6 - Side Referee (original label 4 + 2)
+        "Staff"  # 7 - Staff (original label 5 + 2)
+        # 13 - Maximum possible (original label 11 + 2)
+    ]
+
     box_colors = {
         "0": (150, 50, 50),
         "1": (37, 47, 150),
@@ -273,8 +333,12 @@ if __name__ == "__main__":
         "4": (155, 62, 157),
         "5": (123, 174, 213),
         "6": (217, 89, 204),
-        "7": (22, 11, 15)
+        "7": (22, 11, 15),
+        "8": (255, 0, 0)
+
     }
-    model = YOLO("./weights/last.pt")
-    video_path = sys.argv[1]
+
+    model = YOLO("/content/runs/detect/finetune_v23/weights/best.pt")
+    video_path = "/content/TeamsRefGKBall-14/15sec_input_720p (1)_out.mp4"
     annotate_video(video_path, model)
+
